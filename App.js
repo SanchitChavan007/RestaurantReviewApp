e8e26bad5022a0e0c87693922e21e26bef96c322
Sanchit Chavan <sanchitchavan@Sanchits-MacBook-Pro.local>
2019-06-04 05:26:33 -0400

    Implementing search uisng filter

--- App.js
+++ App.js
@@ -1,49 +1,90 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Text, View, StyleSheet, TextInput} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const restaurants = [
+  {name: 'Taj Restaurant', address: '123 xyz street'},
+  {name: 'Veg Treat', address: '333 abc street'},
+  {name: 'Sahara treat', address: '888 aaa street'},
+  {name: 'Mumbai Spice', address: '000 daa street'}
+]
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+
+  state = {
+search: null
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+     <View style = {{backgroundColor: 'white', flex: 1}}>
+       <Text style = {styles.header}>Restaurant Review</Text>
+       <TextInput style = {styles.input} 
+       placeholder="Live Search"
+       onChangeText={
+         text => {this.setState({search: text})}  
+       }
+       value = {this.state.search}
+         />
+
+       {
+         restaurants.filter(place => {
+           return !this.state.search || place.name.toLowerCase().indexOf(this.state.search.toLowerCase()) > -1
+         }).map((place, index) => {
+         return (
+             <View key = {place.name} style = {[styles.row, {backgroundColor: index % 2 === 0 ? 'white' : '#F3F3F7'}]}>
+               <View style = {styles.edges}>
+               <Text>{index + 1}</Text> 
+               </View>
+               <View style = {styles.nameAddress}>
+               <Text>{place.name}</Text>
+               <Text style = {styles.addressText}>{place.address}</Text>
+               </View>
+               <View style = {styles.edges}>
+              <Text>Info</Text>
+              </View>
+             </View>
+         )
+         })
+       }
+
+     </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
+
+  header: {
+    padding : 40,
+    fontSize: 30,
+    textAlign: 'center',
+    color: '#0066CC',
+    fontWeight: '300' 
+  },
+  row: {
+    flexDirection:'row'
+  },
+  edges: {
+    flex:1,
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center',
+    padding: 5
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  nameAddress: { 
+    flexDirection : 'column', 
+    flex: 8
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  addressText: {
+    color: 'grey'
   },
-});
+  input: {
+    marginBottom: 30,
+    padding: 10,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    color:'#444',
+    borderBottomWidth: 1,
+    borderColor: '#ddd',
+    backgroundColor: '#F5F5F5'
+  }
+})
