a0e87a74a3f516ceb82affa910dd2acc908e8db7
Sanchit Chavan <sanchitchavan@Sanchits-MacBook-Pro.local>
2019-07-24 00:29:02 -0400

    Minor changes

---
+++ src/components/RestaurantList.js
@@ -0,0 +1,95 @@
+import React, {Component} from 'react';
+import { Text, View, StyleSheet, TextInput, Platform, ScrollView, FlatList, Image} from 'react-native';
+
+import Header from '../components/Header';
+
+import axios from 'axios';
+
+import ResImage from '../images/cutlery.png';
+
+import RestaurantRow from '../components/RestaurantRow';
+
+export default class RestaurantList extends Component {
+
+
+state = {
+search: null,
+restaurants: [],
+  }
+
+  componentDidMount() {
+    axios.get('http://opentable.herokuapp.com/api/restaurants?country=US')
+    .then(response => {
+      this.setState({ restaurants: response.data.restaurants});
+    })
+  }
+
+  render() {
+    return (
+     <View style = {{flex: 1}}>
+       <View style ={{ marginTop: 70, alignItems: 'center'}}>
+       <Image source = {ResImage} style = {{ height: 120, width: 120 }}></Image>
+       </View>
+   
+       <Header />
+       <TextInput style = {styles.input} 
+       placeholder="Live Search"
+       onChangeText={
+         text => {this.setState({search: text})}  
+       }
+       value = {this.state.search}
+         />
+
+<FlatList
+          data = {
+            this.state.restaurants.filter(place => {
+              return !this.state.search ||
+                place.name.toLowerCase().indexOf(this.state.search.toLowerCase()) > -1
+            })
+          }
+          renderItem={({ item, index }) => 
+            <RestaurantRow place={item} index={index} />
+          }
+          keyExtractor={item => item.name}
+          initialNumToRender={16}
+        />
+     </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+  header: {
+    padding : 40,
+    fontSize: 30,
+    textAlign: 'center',
+    color: '#0066CC',
+    fontWeight: '300' 
+  },
+  row: {
+    flexDirection:'row'
+  },
+  edges: {
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 5
+  },
+  nameAddress: { 
+    flexDirection : 'column', 
+    flex: 8
+  },
+  addressText: {
+    color: 'grey'
+  },
+  input: {
+    padding: 10,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    color:'#444',
+    borderBottomWidth: 1,
+    borderColor: '#ddd',
+    backgroundColor: '#F5F5F5'
+  }
+})
---
+++ src/components/Stars.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { View } from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const Stars = ({ price }) => { 
+    const stars = [...Array(Math.ceil(price))]
+
+    return (
+        <View style = {{ flexDirection: 'row'}}>
+            {
+                stars.map((_, i) => {
+
+                    const name = Math.floor(price) > i/2 ? 'star' : 'star-half';
+                    return <Icon key = {i} name = {name} color="#FFD64C" />
+                    
+                })
+            }   
+            </View>
+    )
+ }
+
+ export default Stars
